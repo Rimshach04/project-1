3186955ba897fbd12f0fd0375975611826b0350d
RimshaChoudary <rimshachoudhary.7614@gmail.com>
2023-10-25 19:20:52 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AA from './component/AA';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <AA></AA>
+    </div>
+  );
+}
+
+export default App;
