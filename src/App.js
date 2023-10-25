e3e5168da0b0f2bffe78e9d76c5397701253f740
RimshaChoudary <rimshachoudhary.7614@gmail.com>
2023-10-25 20:15:58 +0500

    modifiy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './component/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       
-    
+    <Navbar></Navbar>
     </div>
   );
 }
