8ad563fa7cf5a86aa4208e5ac571bdfbfbf1b6b0
RimshaChoudary <rimshachoudhary.7614@gmail.com>
2023-10-25 19:42:32 +0500

    nav br dd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import AA from './component/AA';
+
 
 function App() {
   return (
     <div className="App">
       
-      <AA></AA>
+    
     </div>
   );
 }
